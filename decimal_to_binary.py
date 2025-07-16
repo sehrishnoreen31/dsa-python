83f04dcb8ef1e36f181678a017f9229dce63c347
Sehrish Noreen <sehrishnoreen31@gmail.com>
2025-07-16 12:55:59 +0500

    updates

--- decimal_to_binary.py
+++ decimal_to_binary.py
@@ -7,6 +7,6 @@ def decimal_to_binary(n):
             remainder = n%2
             binary = str(remainder)+binary
             n = n//2
-    return binary
+        return binary
         
 print(decimal_to_binary(45))
